513bf84f5835eeee462e4ce43e3108521d4d0f2c
guygauravtyagi <gtyagig786@gmail.com>
2022-12-24 23:01:30 +0530

    Node Tree question

---
+++ 100daysofnode/day3_24_dec_2022/index.js
@@ -0,0 +1,15 @@
+const { tree, treeTraversal } = require('./../day1_18_dec_2022/TreeTraversal');
+
+
+findDepth = function (node) {
+    if (!node) return 0;
+    return Math.max(findDepth(node.left), findDepth(node.right)) + 1;
+}
+
+findBalance = function (node) {
+    if (!node) return true;
+    return (findDepth(node.left) - findDepth(node.right) <= 1) && findBalance(node.left) && findBalance(node.right) ? true : false;
+}
+
+
+console.log(findBalance(tree));
