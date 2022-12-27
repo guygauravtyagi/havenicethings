464a9400583c05d2aa015d100e5a8e8e365cf37d
guygauravtyagi <gtyagig786@gmail.com>
2022-12-27 20:30:11 +0530

    Stuff

--- portleo/src/pages/home/Home.js
+++ portleo/src/pages/home/Home.js
@@ -28,84 +28,96 @@ function Home(props) {
             width: '14%',
             top: '24%',
             left: '22%',
-        }} ></Card>
+        }} src='https://images.unsplash.com/photo-1551590192-8070a16d9f67?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80'
+        heading='Abbey' ></Card>
         <Card style={{
             backgroundColor: 'var(--var-not-orange)',
             height: '16%',
             width: '6%',
             top: '84%',
             left: '80%',
-        }} ></Card>
+        }} src='https://images.unsplash.com/photo-1449844908441-8829872d2607?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+        heading='ye sab' ></Card>
         <Card style={{
             backgroundColor: 'var(--var-not-pink)',
             height: '24%',
             width: '12%',
             top: '54%',
             left: '76%',
-        }} ></Card>
+        }} src='https://images.unsplash.com/photo-1670844686624-91d05f20f245?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+        heading='to sahi' ></Card>
         <Card style={{
             backgroundColor: 'var(--var-not-purple)',
             height: '24%',
             width: '12%',
             top: '24%',
             left: '45%',
-        }} ></Card>
+        }} src='https://images.unsplash.com/photo-1671293877875-01ec7b83c2fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'
+        heading='hai par' ></Card>
         <Card style={{
             backgroundColor: 'var(--var-not-violet)',
             height: '20%',
             width: '14%',
             top: '54%',
             left: '30%',
-        }} ></Card>
+        }} src='https://images.unsplash.com/photo-1669545220173-849c2e2d7056?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
+        heading='iska koi' ></Card>
         <Card style={{
             backgroundColor: 'var(--var-not-blue)',
             height: '20%',
             width: '14%',
             top: '84%',
             left: '25%',
-        }} ></Card>
+        }} src='https://images.unsplash.com/photo-1671299741209-04e760c463a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+        heading='fayda' ></Card>
         <Card style={{
             backgroundColor: 'var(--var-not-skyblue)',
             height: '30%',
             width: '16%',
             top: '54%',
             left: '50%',
-        }} ></Card>
+        }} src='https://images.unsplash.com/photo-1664907933800-4a949153ce69?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80'
+        heading='bhi hai' ></Card>
         <Card style={{
             backgroundColor: 'var(--var-not-darkgreen)',
             height: '14%',
             width: '6%',
             top: '94%',
             left: '55%',
-        }} ></Card>
+        }} src='https://images.unsplash.com/photo-1671981151707-7aa791793275?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=689&q=80'
+        heading='ya bas' ></Card>
         <Card style={{
             backgroundColor: 'var(--var-not-green)',
             height: '20%',
             width: '14%',
             top: '34%',
             left: '95%',
-        }} ></Card>
+        }} src='https://images.unsplash.com/photo-1671379827170-aaf624d3ead9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80'
+        heading='dabbey hi' ></Card>
         <Card style={{
             backgroundColor: 'var(--var-not-darkgreen)',
             height: '20%',
             width: '14%',
             top: '24%',
             left: '70%',
-        }} ></Card>
+        }} src='https://images.unsplash.com/photo-1669723009423-6c1b3d11dd92?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+        heading='hilane' ></Card>
         <Card style={{
             backgroundColor: 'var(--var-not-green)',
             height: '14%',
             width: '14%',
             top: '64%',
             left: '92%',
-        }} ></Card>
+        }} src='https://images.unsplash.com/photo-1669612321204-9408aabf8b7b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+        heading='hai...' ></Card>
         <Card style={{
             backgroundColor: 'var(--var-not-darkgreen)',
             height: '20%',
             width: '14%',
             top: '89%',
             left: '95%',
-        }} ></Card>
+        }} src='https://images.unsplash.com/photo-1671282102737-b1fe779a6a05?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+        heading='umar bhar' ></Card>
     </div>
 }
 
--- portleo/src/shared/cards/Card.js
+++ portleo/src/shared/cards/Card.js
@@ -2,7 +2,8 @@ import './Card.css';
 
 function Card(props) {
     return <div className='card-container' style={props.style} >
-        <img src='https://images.unsplash.com/photo-1551590192-8070a16d9f67?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80'></img>
+        <img src={props.src}></img>
+        <h2>{props.heading}</h2>
     </div>
 }
 
