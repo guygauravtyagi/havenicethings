b3cf5c1e09c9086e98597ab56a034c6dfe79ecd9
guygauravtyagi <gtyagig786@gmail.com>
2022-12-31 21:51:22 +0530

    button stuff and other stuff

--- portleo/src/pages/contact/Contact.js
+++ portleo/src/pages/contact/Contact.js
@@ -1,7 +1,15 @@
+import './Contact.css';
+import Button from '../../shared/Button/Button';
+import TextInput from '../../shared/TextInput/TextInput';
+
 function Contact(params) {
     return(
-        <div>
-            <h4>Contact</h4>
+        <div className='contact-container'>
+            <TextInput></TextInput>
+            <div>
+            <Button name='Okay'></Button>
+            <Button name='Email'></Button>
+            </div>
         </div>
     );
 }
---
+++ portleo/src/shared/TextInput/TextInput.js
@@ -0,0 +1,7 @@
+import './TextInput.css';
+
+export default function TextInput(props) {
+    return(
+        <input type='text'/>
+    );
+}
