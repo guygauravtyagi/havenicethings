626216111e4e9ed7ad52aab1be1fb6734afb9c7d
guygauravtyagi <gtyagig786@gmail.com>
2023-01-01 22:18:41 +0530

    Tried with basic view

--- 100daysofnode/day5_1_Jan_2023/Samvaad/index.js
+++ 100daysofnode/day5_1_Jan_2023/Samvaad/index.js
@@ -4,9 +4,34 @@ const app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.json({
-        hello: 'hello'
-    })
+    res.send(`<!DOCTYPE html>
+
+    <head>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.1/socket.io.js"></script>
+        <script src="/chat.js"></script>
+    </head>
+    
+    <body>
+        <h1 class="room-message"></h1>
+        <div class="window">
+            <div class="chat-message">
+                <div id="output"></div>
+                <div id="feedback"></div>
+            </div>
+            <div class='fields'>
+                <input type="text" id="message" placeholder="Enter message">
+                <button id="send">Send</button>
+            </div>
+        </div>
+        <div class="online">
+            <p class="users-online">Users Online</p>
+            <div class="users">
+            </div>
+        </div>
+        <script src="/chat.js"></script>
+    </body>
+    
+    </html>`)
 })
 
 const server = app.listen(port, () => {
