f7b90706a593208d71a0e868aabcf35d20c4c068
guygauravtyagi <gtyagig786@gmail.com>
2023-01-02 21:06:58 +0530

    Added stuff

--- 100daysofnode/day5_1_Jan_2023/samvaad/index.js
+++ 100daysofnode/day5_1_Jan_2023/samvaad/index.js
@@ -2,9 +2,9 @@ const { Server } = require('ws');
 
 const sockserver = new Server({ port: 3000 });
 const connections = new Set();
+
 sockserver.on('connection', (ws) => {
     console.log('New client connected!');
-    console.log(ws);
     connections.add(ws)
     ws.on('message', (data) => {
         const message = JSON.parse(data);
