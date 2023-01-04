d6de0faa2234dcd3e067c4e259e2134423e7f441
guygauravtyagi <gtyagig786@gmail.com>
2023-01-04 21:16:21 +0530

    Frustration push, Can't connect to mongo.

--- 100daysofnode/day5_1_Jan_2023/samvaad/src/index.js
+++ 100daysofnode/day5_1_Jan_2023/samvaad/src/index.js
@@ -1,4 +1,12 @@
 const { Server } = require('ws');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
 
 const socketServer = new Server({ port: 3000 });
 const connections = new Set();
